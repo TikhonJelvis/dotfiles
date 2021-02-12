3c2e76fb1d7460e2fc79afa494f7595189cf8fe1
Tikhon Jelvis <tikhon@jelv.is>
2021-02-11 17:11:06 -0800

    Fixed NDI plugin settings for OBS 26.

--- home/video/default.nix
+++ home/video/default.nix
@@ -6,7 +6,7 @@
   #
   # Could be extended into an "OBS plugin" set of modules, probably...
   home.file.".config/obs-studio/plugins/obs-ndi/bin/64bit/obs-ndi.so".source =
-    "${pkgs.obs-ndi}/lib/x86_64-linux-gnu/obs-plugins/obs-ndi.so";
+    "${pkgs.obs-ndi}/lib/obs-plugins/obs-ndi.so";
   home.file.".config/obs-studio/plugins/obs-ndi/data/locale/en-US.ini".source =
     "${pkgs.obs-ndi}/share/obs/obs-plugins/obs-ndi/locale/en-US.ini";
 }
