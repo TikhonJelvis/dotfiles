f90ee6276501f1f2ceef54211f077ad4ab6b78f4
Tikhon Jelvis <tikhon@jelv.is>
2021-05-20 20:52:20 -0700

    Disabling obs-ndi for now.
    It wasn't working properly for what I wanted anyway...

--- home/firefox/default.nix
+++ home/firefox/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   addons = pkgs.nur.repos.rycee.firefox-addons;
   inherit (addons) buildFirefoxXpiAddon;
@@ -12,7 +12,7 @@ let
     addonId = "{73a6fe31-595d-460b-a920-fcc0f8843232}";
     url = "https://addons.mozilla.org/firefox/downloads/file/3629456/noscript_security_suite-11.0.39-an+fx.xpi?src=";
     sha256 = "0pkvgw7bi1isnf4bfb6jcgjwlnidxz8c4s48ryc29shlxqmy9bfh";
-    meta = with pkgs.stdenv.lib; {
+    meta = with lib; {
       homepage = "https://noscript.net/";
       description = "Noscript.";
       license = licenses.gpl2;
