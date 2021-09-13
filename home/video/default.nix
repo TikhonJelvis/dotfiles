0dfad3b00a4d4ec06730158aced27f0888b43d8b
Tikhon Jelvis <tikhon@jelv.is>
2021-09-13 10:50:37 -0700

    Reenabled OBS-NDI with new OBS plugin infrastructure.
    Looks like it still doesn't work correctly over my network though :/x

--- home/video/default.nix
+++ home/video/default.nix
@@ -1,12 +1,11 @@
 { pkgs, ... }:
+let
+  obs = pkgs.wrapOBS {
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-ndi
+    ];
+  };
+in
 {
-  home.packages = with pkgs; [ obs-studio ];
-
-  # Managing the OBS-NDI plugin.
-  #
-  # Could be extended into an "OBS plugin" set of modules, probably...
-  # home.file.".config/obs-studio/plugins/obs-ndi/bin/64bit/obs-ndi.so".source =
-  #   "${pkgs.obs-ndi}/lib/obs-plugins/obs-ndi.so";
-  # home.file.".config/obs-studio/plugins/obs-ndi/data/locale/en-US.ini".source =
-  #   "${pkgs.obs-ndi}/share/obs/obs-plugins/obs-ndi/locale/en-US.ini";
+  home.packages = [ obs ];
 }
