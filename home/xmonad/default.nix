c5bcf01087e9f2062c64938f1265f19bd65be467
Tikhon Jelvis <tikhon@jelv.is>
2021-09-13 17:12:55 -0700

    Fix XMonad by setting up a background image

--- home/xmonad/default.nix
+++ home/xmonad/default.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 
+let
+  breeze-wallpaper = "${pkgs.breeze-qt5}/share/wallpapers/Next/contents/images/2560x1440.png";
+in
 {
   xsession = {
     enable = true;
@@ -9,6 +12,12 @@
       enableContribAndExtras = true;
       config = ./xmonad.hs;
     };
+
+    initExtra = "feh --bg-fill ${breeze-wallpaper}";
+  };
+
+  programs.feh = {
+    enable = true;
   };
 
   home.packages = with pkgs; [ dmenu xorg.xmessage ];
