692b34abc9440d214276b9b3549e83640862fa9b
Tikhon Jelvis <tikhon@jelv.is>
2021-09-17 20:37:38 -0700

    Enable Discord on NixOS

--- home/base/default.nix
+++ home/base/default.nix
@@ -15,10 +15,11 @@
   home = {
     packages = with pkgs;
       let
+        apps         = [ discord ];
         utils        = [ drive pandoc unzip zip ];
         development  = [ ghc lorri niv python3 jupyter poetry ];
         aspell       = pkgs.aspellWithDicts (d: [d.en d.ru]);
-      in utils ++ development ++ [ aspell ];
+      in apps ++ utils ++ development ++ [ aspell ];
 
     sessionPath = [
       "$HOME/.nix-profile/bin"
