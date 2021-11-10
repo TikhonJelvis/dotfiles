5a62a10092324720ec5195d85f0a20407924010e
Tikhon Jelvis <tikhon@jelv.is>
2021-11-09 20:32:25 -0800

    Configured Borg for backups on NixOS

--- nixos/berkeley.nix
+++ nixos/berkeley.nix
@@ -32,6 +32,44 @@
     banner = "tikhon-berkeley-nixos";
   };
 
+  services.borgbackup.jobs = {
+    "borgbase" = {
+      paths = [
+        "/home"
+      ];
+      exclude = [
+        # XDG cache directory
+        "/home/*/.cache"
+
+        # Bigger app data directories (> 250M)
+        "/home/*/.local/share/Trash"
+        "/home/*/.local/share/baloo"
+        "/home/*/.local/share/Steam"
+
+        # Build outputs
+        "**/target"        # Rust, Scala... etc
+        "/home/*/.cargo"   # Global Rust cache
+        "**/dist"          # Python, Haskell... etc
+        "**/dist-newstyle" # Haskell
+        "**/_site"         # Hakyll
+        "**/_cache"        # Hakyll
+
+        # Big projects
+        "**/Programming/nixpkgs"
+      ];
+      repo = "h05ln3up@h05ln3up.repo.borgbase.com:repo";
+      encryption = {
+        mode = "repokey-blake2";
+        passCommand = "cat /root/borg-passphrase";
+        # Passphrase also saved in 1Password as
+        # tikhon-nixos-berkeley-passphrase under borgbase
+      };
+      environment.BORG_RSH = "ssh -i /root/.ssh/borgbase";
+      compression = "auto,zstd";
+      startAt = "daily";
+    };
+  };
+
   boot = {
     loader = {
       systemd-boot.enable = true;
