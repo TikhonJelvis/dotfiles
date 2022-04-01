60ca201d6c9fb15454c50707e6da1aedf9148e63
Tikhon Jelvis <tikhon@jelv.is>
2022-03-31 20:56:23 -0700

    Bumped Nixpkgs and fixed obs-studio code
    I'm not using SRT after all (NDI just seems to work better) and it's apparently enabled in `ffmpeg` by default anyhow.

--- home/video/default.nix
+++ home/video/default.nix
@@ -1,17 +1,6 @@
 { pkgs, lib, ... }:
 let
-  ffmpeg-srt = pkgs.ffmpeg.overrideAttrs (oldAttrs: rec {
-    configureFlags = oldAttrs.configureFlags ++ [ "--enable-libsrt" ];
-    buildInputs = oldAttrs.buildInputs ++ [ pkgs.srt ];
-  });
-
-  wrapOBS = pkgs.wrapOBS.override {
-    obs-studio = pkgs.obs-studio.override {
-      ffmpeg = ffmpeg-srt;
-    };
-  };
-
-  obs-studio = wrapOBS {
+  obs-studio = pkgs.wrapOBS {
     plugins = with pkgs.obs-studio-plugins; [
       obs-ndi
     ];
