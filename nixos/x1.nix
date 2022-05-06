b6a34abebc9bf19c2fd2327f9b4554405452437d
Tikhon Jelvis <tikhon@jelv.is>
2022-05-06 13:14:00 -0700

    Set up X1 laptop with NixOS + home-manager

--- nixos/x1.nix
+++ nixos/x1.nix
@@ -1,12 +1,32 @@
 { config, pkgs, ... }:
 {
-  imports = [ ./base ];
+  imports = [ ./base ./hardware-configuration/x1.nix ];
 
   networking = {
     hostName = "tikhon-nixos-x1";
 
     networkmanager.enable = true;
+
+    useDHCP = false;
+    interfaces.enp0s31f6.useDHCP = true;
+    interfaces.wlp0s20f3.useDHCP = true;
   };
 
+  users.mutableUsers = false;
   users.users.tikhon.extraGroups = [ "networkmanager" ];
+  users.users.tikhon.hashedPassword = "$6$CHDRwI4R8XXAj131$o.gcXEondaWAYr4bipBbU5C0fofuSuApe5eJEASsLCBgHY30sZ9qKL96hMGXAV7GssNJzcgaPHCZpsOSj4yju0";
+
+  # touch pad
+  services.xserver.libinput = {
+    enable = true;
+
+    clickMethod = "clickfinger";
+    tapping = false;
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  system.stateVersion = "21.11";
 }
