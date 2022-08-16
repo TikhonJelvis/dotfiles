c08a6c05622b441d19f98a0ea66a7bd1d91cd513
Tikhon Jelvis <tikhon@jelv.is>
2022-08-15 22:16:23 -0700

    Improved Framework laptop NixOS configuration

--- nixos/framework.nix
+++ nixos/framework.nix
@@ -24,5 +24,24 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # kernel settings as suggested in
+  # nixos-hardware/framework/default.nix
+  boot.kernelParams = [
+    # better power consumption
+    "mem_sleep_default=deep"
+    "nvme.noacpi=1"
+  ];
+
+  # need at least 5.16 for Wifi + Bluetooth
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # fingerprint reader
+  services.fprintd.enable = true;
+
+  # for changing monitor brightness
+  #
+  # (mis-detected by nixos-generate-config)
+  hardware.acpilight.enable = true;
+
   system.stateVersion = "22.05";
 }
