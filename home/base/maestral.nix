fedcf7e1fc2413eb5a092230af8bfba82ed4e4a8
Tikhon Jelvis <tikhon@jelv.is>
2023-08-24 18:03:25 -0700

    Fixed maestral service config

--- home/base/maestral.nix
+++ home/base/maestral.nix
@@ -6,15 +6,6 @@
 { config, lib, pkgs, ... }:
 let cfg = config.services.maestral;
     inherit (lib) mkEnableOption mkIf mkMerge mkOption types;
-    ini = pkgs.formats.ini { };
-
-    maestral-ini = ini.generate "maestral-override.ini" {
-      app = {
-        analytics = "False"; # "False" has to be capitalized
-        notification_level = 30; # SYNCISSUE
-        update_notification_interval = 0;
-      };
-    };
 in {
   options = {
     services.maestral = {
@@ -31,23 +22,10 @@ in {
         Install = { WantedBy = [ "default.target" ]; };
 
         Service = {
-          Type = "forking";
-          PIDFile = "${config.home.homeDirectory}/.maestral/maestral.pid";
-
+          ExecStart = "${pkgs.maestral}/bin/maestral start -f";
+          ExecStop = "${pkgs.maestral}/bin/maestral stop";
           Restart = "on-failure";
-          PrivateTmp = true;
-          ProtectSystem = "full";
           Nice = 10;
-
-          ExecStartPre = ''
-          /bin/sh -c '${pkgs.crudini}/bin/crudini --merge $HOME/.config/maestral/maestral.ini < ${maestral-ini}'
-        '';
-          ExecStart = "${lib.getBin pkgs.maestral}/bin/maestral start --foreground";
-          ExecStop = "${lib.getBin pkgs.maestral}/bin/maestral stop";
-          ExecReload = "${lib.getBin pkgs.coreutils}/bin/kill -HUP $MAINPID";
-          
-          WatchdogSec = "60s";
-          RuntimeDirectory = "maestral";
         };
       };
     };
