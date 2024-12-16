c993853eae3889c29ce10be5ccd3db191bb98b64
Tikhon Jelvis <tikhon@jelv.is>
2024-12-16 09:33:17 -0800

    Switch to obs-teleport from obs-ndi/distav

--- home/video/default.nix
+++ home/video/default.nix
@@ -1,26 +1,6 @@
 { pkgs, lib, ... }:
 let
-  # for reference; not currently using
-  #
-  # move into plugs = with pkgs.obs-studio-plugins; [ ... ]
-  #
-  # ndi = (obs-ndi.override {
-  #   ndi = pkgs.ndi.overrideAttrs (attrs: rec {
-  #     src = pkgs.fetchurl {
-  #       name = "${attrs.pname}-${attrs.version}.tar.gz";
-  #       url = "https://go.ndi.video/e/428312/nstall-NDI-SDK-v5-Linux-tar-gz/79rns7/1238777648?h=R1GqlmeJwHONmT1otjh-L-ykzTvd_VKP4as3DhcuDRI";
-  #       hash = "sha256-HPzDLuJrwlccXL9x6B2vxnbjiH5XJKic5Qj0njxeBXI=";
-  #     };
-
-  #     unpackPhase = ''
-  #           unpackFile ${src}
-  #           echo y | ./${attrs.installerName}.sh
-  #           sourceRoot="NDI SDK for Linux"
-  #         '';
-  #   });
-  # });
   obs-studio = pkgs.wrapOBS {
-    # TODO: update to DistAv?
     plugins = with pkgs.obs-studio-plugins; [
       obs-teleport
     ];
