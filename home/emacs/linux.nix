57da0887601ba00ef000ad8cea32658fa92dcba5
Tikhon Jelvis <tikhon@jelv.is>
2024-12-18 22:04:53 -0800

    Fixed emacsclient in my home-manager setup!

--- home/emacs/linux.nix
+++ home/emacs/linux.nix
@@ -16,6 +16,18 @@ in {
   services.emacs = {
     enable = true;
     client.enable = true;
+
+    # 2024-12-18: emacsclient stopped being able to open GUI
+    # windows. Some searching identified the likely cause: the Emacs
+    # server gets launched before X and does not have access to
+    # DISPLAY environment variables[1].
+    #
+    # Looking through the home-manager emacs service definition, it
+    # seems like this should force the emacs server service to start
+    # *after* X.
+    #
+    # [1]: https://unix.stackexchange.com/questions/728058/systemd-starts-emacs-daemon-without-xauthority-environment-variable-after-upgrad
+    startWithUserSession = "graphical";
   };
 
   home.file = extra-packages;
