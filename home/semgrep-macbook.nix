6fc0f51f220936422becd140065dc7fe2658d13b
Tikhon Jelvis <tikhon@semgrep.com>
2025-06-02 14:16:05 -0700

    semgrep-specific home-manager settings

---
+++ home/semgrep-macbook.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ./base
+    ./emacs/darwin.nix
+  ];
+
+  home = {
+    username = "tikhon";
+    homeDirectory = "/Users/tikhon";
+  };
+
+  programs.git = {
+    ignores = [ ".DS_Store" ];
+
+    userEmail = "tikhon@semgrep.com";
+  };
+}
