cbf650c6e20afa2ef323e328dd482080a5f632d8
Tikhon Jelvis <tikhon@semgrep.com>
2025-06-04 13:42:00 -0700

    ocaml lsp settings

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -1800,13 +1800,16 @@ consistently fail on them."
   :ensure t
   :hook
   ((tuareg-mode . tikhon/atd-no-format-hook)
-   (tuareg-mode . lsp))
+   (tuareg-mode . tikhon/ocaml-lsp))
   :config
   (add-to-list 'auto-mode-alist '("\\.atd\\'" . tuareg-mode))
   :init
   (defun tikhon/atd-no-format-hook ()
     (when (equal (file-name-extension (buffer-file-name)) "atd")
-      (format-all-mode -1))))
+      (format-all-mode -1)))
+  (defun tikhon/ocaml-lsp ()
+    (unless (equal (file-name-extension (buffer-file-name)) "atd")
+      (lsp))))
 
                                         ; LEAN
 ;;; Lean mode is not loading correctly for some reason; will debug
