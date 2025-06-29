f094d24dd1222609fad22db7d963993ff35b1f56
Tikhon Jelvis <tikhon@jelv.is>
2025-06-29 10:40:52 -0700

    Use explicit shellHook for setting environment variables
    For some reason, the previous approach I had stopped working. Or maybe it never worked? Figuring this out would be too much work, so I'm falling back on a moderately awkward `shellHook` version instead.

--- shell.nix
+++ shell.nix
@@ -1,13 +1,17 @@
 { sources ? import ./nix/sources.nix
 , pkgs    ? import sources.nixpkgs {}
 }:
-pkgs.mkShell rec {
-  buildInputs = [ pkgs.cachix pkgs.niv pkgs.home-manager ];
-
+let
   HOME_MANAGER_CONFIG =
     if pkgs.stdenv.isDarwin
     then toString ./home/semgrep-macbook.nix
     else toString ./home/linux.nix;
-
   NIX_PATH = "nixpkgs=${sources.nixpkgs}:nixpkgs/nixos=${sources.nixpkgs}:nixos-config=/etc/nixos/configuration.nix";
+in pkgs.mkShell rec {
+  buildInputs = [ pkgs.cachix pkgs.niv pkgs.home-manager ];
+
+  shellHook = ''
+    export HOME_MANAGER_CONFIG='${HOME_MANAGER_CONFIG}'
+    export NIX_PATH='${NIX_PATH}'
+  '';
 }
