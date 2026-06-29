312b6561a98dff241b2a80cd93180880267a446a
Tikhon Jelvis <tikhon@jelv.is>
2026-06-29 15:55:59 -0700

    add postgres client by default

--- home/base/default.nix
+++ home/base/default.nix
@@ -18,7 +18,7 @@
         utils =
           [ drive pandoc unzip zip _1password-cli poppler-utils aspell ]; # TODO: fix ptouch-print on macos?
         development  =
-          [ ghc lorri niv python3 poetry cachix stylish-haskell python3Packages.livereload uv ];
+          [ ghc lorri niv python3 poetry cachix stylish-haskell python3Packages.livereload uv postgresql ];
         aspell = pkgs.aspellWithDicts (d: [d.en d.ru]);
       in utils ++ development;
 
